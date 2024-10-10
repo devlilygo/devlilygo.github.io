60b3b5550a3e236f915fd64156663790b5ed7b15
Lily <devlilygo@gmail.com>
2024-10-10 15:57:44 -0400

    feat: 글씨체 추가

--- src/components/organisms/Introduction/IntroductionView.tsx
+++ src/components/organisms/Introduction/IntroductionView.tsx
@@ -2,12 +2,26 @@ import classNames from "classnames";
 
 const style = {
   Container: classNames(
-    "flex flex-col h-screen w-full items-center justify-center bg-slate-500"
+    "flex flex-col h-screen w-full items-center justify-center bg-sky-200 font-[Cafe24MeongiB] text-9xl text-white "
   ),
+  SubLabel: classNames("font-[Cafe24MeongiW] text-4xl"),
 };
 
 const IntroductionView = () => {
-  return <div className={style.Container}>Introductions</div>;
+  return (
+    <div
+      className={style.Container}
+      style={{ textShadow: "2px 3px 3px rgba(0, 0, 0, 0.15)" }}
+    >
+      Lily Go
+      <div
+        className={style.SubLabel}
+        style={{ textShadow: "2px 3px 3px rgba(0, 0, 0, 0.15)" }}
+      >
+        Front-End Developer
+      </div>
+    </div>
+  );
 };
 
 export default IntroductionView;
