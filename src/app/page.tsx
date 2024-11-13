2375734d90092d006ba1be902bff4b39156aa08d
Lily <devlilygo@gmail.com>
2024-11-13 15:26:33 -0500

    feat: 새로운 디렉토리 구조 (오류수정)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import HomeTemplate from "@/components/templates/HomeTemplate";
+import MainLayout from "@/components/templates/MainLayout";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen w-full items-center justify-between">
-      <HomeTemplate />
+      <MainLayout />
     </div>
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     //   <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
---
+++ src/components/templates/MainLayout.tsx
@@ -0,0 +1,11 @@
+const MainLayout = () => {
+  return (
+    <div className="flex flex-col h-full w-full overflow-y-auto">
+      {/* <Introduction />
+      <Career /> */}
+      hello
+    </div>
+  );
+};
+
+export default MainLayout;
