ade1f3dedd25ae49afc3977af55e2911dfb5f288
Lily <devlilygo@gmail.com>
2025-04-04 06:17:16 -0400

    chore: change favicon & bg - chore: change favicon & bg

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -14,15 +14,23 @@ import TaskCard from "../organisms/TaskCard/TaskCard";
 import { MessagesCard } from "../organisms/MessagesCard/MessagesCard";
 import { EmojiCalendarCard } from "../organisms/EmojiCalendarCard/EmojiCalendarCard";
 import { SkillsWorksCard } from "../organisms/SkillsWorksCard/SkillsWorksCard";
+import Image from "next/image";
 
 const styles = {
   container: classNames(
     "flex",
+    "flex-col",
+    "h-full",
+    "w-full",
+    "overflow-hidden"
+  ),
+  mainContainer: classNames(
+    "flex",
+    "flex-col",
     "h-full",
     "w-full",
     "items-center",
-    "justify-center",
-    "bg-background"
+    "justify-center"
   ),
   cardContainer: classNames(
     "w-[1024px]",
@@ -37,39 +45,63 @@ const styles = {
   cardContentRow1: classNames("flex", "w-full", "h-full", "gap-4"),
   cardContentRow2: classNames("flex", "w-full", "h-full", "gap-4"),
   cardFooter: classNames("flex", "justify-between"),
+  footerContainer: classNames(
+    "absolute",
+    "bottom-0",
+    "flex",
+    "flex-col",
+    "h-[100px]",
+    "w-full",
+    "items-center",
+    "justify-start"
+  ),
 };
 
 //NOTE - 랜딩 페이지 (Dashboard)
 const HomeTemplate = () => {
   return (
     <div className={styles.container}>
-      <Card className={styles.cardContainer}>
-        <CardHeader className={styles.cardHeader}>
-          <CardTitle className={styles.cardTitle}>
-            <span className="text-6xl font-handwriting">W</span>elco
-            <span className="text-6xl font-handwriting">m</span>e to
-            <span className="text-6xl font-handwriting">D</span>
-            ailil<span className="text-6xl font-handwriting">y</span> 🌷
-            <CardDescription className="italic">
-              Daily Lily’s personal space for tiny wins
+      <div className={styles.mainContainer}>
+        <Card className={styles.cardContainer}>
+          <CardHeader className={styles.cardHeader}>
+            <CardTitle className={styles.cardTitle}>
+              <div className="flex items-center">
+                <span className="text-6xl font-handwriting">W</span>elco
+                <span className="text-6xl font-handwriting">m</span>e to
+                <span className="text-6xl font-handwriting">D</span>
+                ailil<span className="text-6xl font-handwriting">y</span>
+                <Image
+                  className="ml-2 pb-2"
+                  alt="flower"
+                  src="/images/flower.png"
+                  width={50}
+                  height={50}
+                />
+              </div>
+              <CardDescription className="italic">
+                Daily Lily’s personal space for tiny wins
+              </CardDescription>
+            </CardTitle>
+            <CardDescription className="text-muted-foreground">
+              {format(new Date(), "EEE MMMM d, yyyy").toUpperCase()}
             </CardDescription>
-          </CardTitle>
-          <CardDescription className="text-muted-foreground">
-            {format(new Date(), "EEE MMMM d, yyyy").toUpperCase()}
-          </CardDescription>
-        </CardHeader>
-        <CardContent className={styles.cardContent}>
-          <div className={styles.cardContentRow1}>
-            <TaskCard />
-            <MessagesCard />
-          </div>
-          <div className={styles.cardContentRow2}>
-            <EmojiCalendarCard />
-            <SkillsWorksCard />
-          </div>
-        </CardContent>
-        <CardFooter className={styles.cardFooter}></CardFooter>
-      </Card>
+          </CardHeader>
+          <CardContent className={styles.cardContent}>
+            <div className={styles.cardContentRow1}>
+              <TaskCard />
+              <MessagesCard />
+            </div>
+            <div className={styles.cardContentRow2}>
+              <EmojiCalendarCard />
+              <SkillsWorksCard />
+            </div>
+          </CardContent>
+          <CardFooter className={styles.cardFooter}></CardFooter>
+        </Card>
+      </div>
+      <div className={styles.footerContainer}>
+        <Image src="/images/dailily.png" alt="logo" width={100} height={100} />
+      </div>
     </div>
   );
 };
