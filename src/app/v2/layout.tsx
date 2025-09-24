04ef82a4f6ae9d5a8e4a39a05bf6bdf3f347fb03
Lily <devlilygo@gmail.com>
2025-09-24 13:03:08 -0400

    chore: replace directories v2 to v3 system

---
+++ src/app/v2/layout.tsx
@@ -0,0 +1,23 @@
+import { Toaster } from "@/components/ui/sonner";
+import "../globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Lily Go | Frontend Developer",
+  description: "Frontend Developer",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
---
+++ src/components/v3/templates/MainTemplate.tsx
@@ -0,0 +1,5 @@
+const MainTemplate = () => {
+  return <div>main v3.0</div>;
+};
+
+export default MainTemplate;
